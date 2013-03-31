6d34e23b0c14827ea10d20cd0c9c60f13b646ede
Michel Albert <michel@albert.lu>
2013-03-31 18:00:36 +0200

    No more module-level code, input encoding.

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import csv
 from collections import namedtuple
+import codecs
 
 DataRow = namedtuple('DataRow',
     'accounting_date, '
@@ -14,7 +15,7 @@ DataRow = namedtuple('DataRow',
     'operation_reference')
 
 def to_qif(record):
-    return """D{accounting_date}
+    return u"""D{accounting_date}
 T{amount}
 N{operation_reference}
 M{communication_1} {communication_2}
@@ -22,12 +23,52 @@ P{counterparty_name} ({counterparty_account})
 ^
 """.format(**record)
 
-with open('input.csv', 'rb') as csvfile:
-    with open('out.qif', 'w') as outfile:
-        outfile.write('!Type:Bank\n')
-        acc_info = csvfile.readline()
-        header = csvfile.readline()
-        csvreader = csv.reader(csvfile, delimiter=';', quotechar='"')
-        for row in csvreader:
-            record = DataRow(*row)
-            outfile.write(to_qif(record._asdict()))
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+
+def convert(source_filename, target_filename):
+    with open(source_filename, 'r') as csvfile:
+        with codecs.open(target_filename, 'w', encoding='utf8') as outfile:
+            outfile.write(u'!Type:Bank\n')
+            acc_info = csvfile.readline()
+            header = csvfile.readline()
+            csvreader = UnicodeReader(csvfile, delimiter=';', quotechar='"',
+                encoding='latin1')
+            for row in csvreader:
+                record = DataRow(*row)
+                outfile.write(to_qif(record._asdict()))
+                print u'Wrote {0.accounting_date} - {0.communication_1}'.format(
+                        record).encode('utf8')
+
+
+if __name__ == "__main__":
+    convert('input.csv', 'out.qif')
