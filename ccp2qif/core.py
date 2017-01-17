8459d92fe07e7d68eea515c4cec5cbd841d2822e
Michel Albert <michel@albert.lu>
2017-01-17 20:42:59 +0100

    Better account number detection.

--- ccp2qif/core.py
+++ ccp2qif/core.py
@@ -2,7 +2,9 @@ from __future__ import print_function
 from collections import namedtuple
 import codecs
 import sys
-from os.path import splitext
+from os.path import splitext, basename
+
+from schwifty import IBAN
 
 from ccp2qif.util import UnicodeReader
 
@@ -43,38 +45,63 @@ P{counterparty_}
 
 
 def account_name_from_filename(filename):
-    from schwifty import IBAN
     # if the filename is a valid IBAN number, we take this as account number
-    base_name, _, _ = filename.rpartition('.')
+    base_name, _, _ = basename(filename).rpartition('.')
     try:
         iban = IBAN(base_name)
     except ValueError:
         # not a valid IBAN number. We can ignore this.
         return None
     else:
-        return iban.compact
+        return iban.formatted
+
+
+def try_getting_account_number(line):
+    if line.lower().startswith('account number'):
+        account_info = line.split(';')
+        raw_account_number = account_info[1]
+        try:
+            account_number = IBAN(raw_account_number)
+        except ValueError:
+            return None
+        else:
+            return account_number.formatted
+    else:
+        return None
 
 
 def convert(source_filename, target_filename, account_name=None):
     detected_account_name = account_name_from_filename(source_filename)
     if not account_name and detected_account_name:
-        print('Using %s as accound number (from filename): ' %
+        print('Using %s as account number (from filename): ' %
               detected_account_name)
         account_name = detected_account_name
     elif account_name:
-        print('Using %s as accound number (from CLI argument): ' %
+        print('Using %s as account number (from CLI argument): ' %
               account_name)
+    else:
+        print('No account number manually specified')
 
     with open(source_filename, 'r') as csvfile:
         with codecs.open(target_filename, 'w', encoding='utf8') as outfile:
+
+            # If the file contains a line with account info, this overrides the
+            # rest.
+            account_line = csvfile.readline()
+            account_number = try_getting_account_number(account_line)
+            if account_number:
+                print('Account number %r found in file. Overriding manual '
+                      'value' % account_number)
+                account_name = account_number
+
             if account_name:
                 outfile.write('!Account\n')
                 outfile.write('N{0}\n'.format(account_name))
                 outfile.write('TBank\n')
                 outfile.write('^\n')
             outfile.write(u'!Type:Bank\n')
-            acc_info = csvfile.readline()
-            header = csvfile.readline()
+
+            csvfile.readline()  # skip header
             csvreader = UnicodeReader(csvfile, delimiter=';', quotechar='"',
                                       encoding='latin1')
             for row in csvreader:
